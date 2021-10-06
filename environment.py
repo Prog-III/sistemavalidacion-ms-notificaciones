a87b907eb24d7ea5cc06425d2f8e06f4fc42e969
Albert Muñoz <albert.1701920351@ucaldas.edu.co>
2021-10-05 19:04:40 -0500

    Creacion del servidor y el environment adaptado a notificaciones por correo

---
+++ environment.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  5 13:16:35 2021
+
+@author: WOLF
+"""
+
+import os
+
+#OJO CON EL API KEY EN GITHUB
+os.environ["SENDGRID_API_KEY"]= "Aqui viene la api Key de sendgrid"
+os.environ["SECURITY_HASH"]="LU3RRjfXHvvl"
+os.environ["email_from"]="albertmunoz131@gmail.com"
